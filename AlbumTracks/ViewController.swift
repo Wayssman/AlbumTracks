5190f6accf61ef55a65a1a3953894fd24af85cc8
Alexandr <AlexandrZhelanov@gmail.com>
2021-05-04 17:57:25 +0300

    Initial Commit

---
+++ AlbumTracks/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AlbumTracks
+//
+//  Created by Желанов Александр Валентинович on 04.05.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
